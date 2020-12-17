e69cd614184ee08c4b5332d5bc21a536a49aa111
Jonas Gregorio <jonas.gregorio@gmail.com>
2020-12-17 21:51:21 +0100

    update test

--- test.py
+++ test.py
@@ -21,20 +21,24 @@ def show_bar(it, size):
         print("\n")
 
 
-SETTINGS = [("arawak", 4400, "latia", 0.224, 0.0, "arawak_sw"),
-            ("arawak", 4400, "latia", 0.224, 0.5, "arawak_sw"),
+SETTINGS = [("arawak", 4100, "latia", 0.224, 0.0, "arawak_sw"),
+            ("arawak", 4100, "latia", 0.224, 0.5, "arawak_sw"),
             ("arawak", 3000, "saladero", 0.224, 0.0, "arawak_short"),
             ("arawak", 3000, "saladero", 0.224, 0.5, "arawak_short"),
-            ("tupi", 4100, "encontro", 0.244, 0.0, "tupi_sw"),
-            ("tupi", 4100, "encontro", 0.244, 0.5, "tupi_sw"),
+            ("tupi", 4400, "encontro", 0.244, 0.0, "tupi_sw"),
+            ("tupi", 4400, "encontro", 0.244, 0.5, "tupi_sw"),
             ("tupi", 2500, "bvista", 0.244, 0.0, "tupi_e"),
             ("tupi", 2500, "bvista", 0.244, 0.5, "tupi_e")]
 
 
+"""
 fiss_vals = [i for i in range(50, 151, 25)]
 k_vals = [i for i in range(20, 101, 20)]
 leap_vals = [i for i in range(0, 31, 10)]
-
+"""
+fiss_vals = [i for i in range(50, 151, 50)]
+k_vals = [i for i in range(20, 101, 40)]
+leap_vals = [i for i in range(0, 31, 15)]
 
 for SETTING in SETTINGS:
     CULTURE = SETTING[0]
@@ -61,7 +65,7 @@ for SETTING in SETTINGS:
             for leap_val in leap_vals:
                 combs.append([fiss_val, k_val, leap_val])
 
-    pool = mp.Pool(18)
+    pool = mp.Pool(3)
     res = pool.map(run, combs)
     pool.close()
 
